a3b46fcdb7397622bf7b5d569a3e6b379a0fd8eb
Austin <austinrhodes462@gmail.com>
2025-01-08 19:17:15 -0500

    "Add code for spotify api code challenge"

--- Program.cs
+++ Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string codeVerifier = GenerateRandomString(64);
+        Console.WriteLine($"Code Verifier: {codeVerifier}");
+        var codeChallenge = GenerateSha256(codeVerifier);
+    }
+
+    static string GenerateRandomString(int length)
+    {
+        const string possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        var randomBytes = new byte[length];
+        using (var rng = RandomNumberGenerator.Create())
+        {
+            rng.GetBytes(randomBytes);
+        }
+
+        var stringBuilder = new StringBuilder(length);
+        foreach (var b in randomBytes)
+        {
+            stringBuilder.Append(possible[b % possible.Length]);
+        }
+
+        return stringBuilder.ToString();
+    }
+
+    static string GenerateSha256(string codeVerifier)
+    {
+        using (var sha256 = SHA256.Create())
+        {
+            var bytes = Encoding.UTF8.GetBytes(codeVerifier);
+            var hash = sha256.ComputeHash(bytes);
+            return Convert.ToBase64String(hash);
+        }
+    }
+}
